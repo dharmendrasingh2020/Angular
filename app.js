c3d2b2201b06809e30b9f89a026cd4c9e1213cb5
dharmendrasingh2020 <67416349+dharmendrasingh2020@users.noreply.github.com>
2020-07-01 09:36:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-( function(){
+(function() {
     'use strict';
-    angular.module('NameCalculator',[])
-    .controller('NameCalculatorController', function ($scope)
-    {
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
 
-       $scope.name =" ";
-        $scope.totalValue = 0;
-        $scope.displayNeumeric =funtion()
-        {
-            var totalNameValue= calculatNumericForString($scope.name);
-            $scope.totalValue= totalNameValue;
-        };
+    function LunchCheckController($scope) {
+        $scope.lunchList = "";
+        $scope.message = "";
+        $scope.helpMessage = "";
 
-             funtion calculatNumericForString(sting)
-             {
-            var totalStringValue= 0;
-            for (var i=0;i < String.length; i++){
-                totalStringValue += String.charCpdeAt(i);
-
-            }
-return totalStringValue;
+        function setAlertStyle(color) {
+            $scope.inputStyle = {
+                'border-color': color
+            };
+            $scope.mesageStyle = {
+                'color': color
+            };
         }
-        
-    });
-
+        $scope.checkIfTooMuch = function() {
+            var lunchList = $scope.lunchList;
+            if (lunchList.length > 0) {
+                var count = 0;
+                var lunchArr = lunchList.split(",");
+                var lunchArrSize = lunchArr.length;
+                for (var i = 0; i < lunchArrSize; i++) {
+                    if (lunchArr[i].trim().length > 0) {
+                        count++;
+                    }
+                }
+                setAlertStyle('#398439');
+                $scope.message = count > 3 ? "Too much!" : "Enjoy!";
+                $scope.helpMessage = count < lunchArrSize ? "I don't consider empty item!" : "";
+            } else {
+                setAlertStyle('#B5555A');
+                $scope.message = "Please enter data first";
+            }
+        };
+    }
 })();
- 
